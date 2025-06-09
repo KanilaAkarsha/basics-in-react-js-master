efe3966d5dcb3a4edc7136de963cb0139a5bbcd8
KanilaAkarsha <Kanilasaman1@gmail.com>
2025-06-09 12:33:17 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 var userEmail = "kanilasaman1@gmail.com";
-const userName = "Kanila Akarsha";
-let userAge = 22;
+const userName = "Kanila Akarsha";//string
+let userAge = 22; //number
+const isAuthenticated = false; //boolean
+const lastName = null; //null
+const firstName = undefined; //undefined
+const userData = {
+    firstName : "Kanila",
+    lastName : "Akarsha",
+    userAge : 23
+};//object
+const fruits =["banana","apple","guava"]; //array
+const number = [1,2,3,4,5];
+const users = [
+    {id: 1,name:"kanila"},
+    {id: 2,name:"Akarsha"}
+];
 
 userAge = 23;
 
 console.log("I'M first javascript file here",{userEmail,userName,userAge});
+console.log(userAge,typeof userAge)
+console.log("value",isAuthenticated,"type: ",typeof isAuthenticated)
+console.log(lastName,typeof lastName)
+console.log(firstName,typeof firstName)
+console.log(userData,typeof userData)
+console.log(fruits,typeof fruits)
+console.log(users,typeof users)
